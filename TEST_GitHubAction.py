602a7aac205189614b7c4984243e02c741baf4d2
shovanpaul48 <shovanpaul48@gmail.com>
2024-03-10 20:10:31 +0530

    Prime Check Function added

--- TEST_GitHubAction.py
+++ TEST_GitHubAction.py
@@ -3,7 +3,17 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)
+    
+def CheckPrime(n):
+
+    if n <= 1:
+        return "Prime"
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return "Not Prime"
+    return "Prime"
 
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
     print("Factorial of", num, "is", factorial(num))
+    print(num, "is ", CheckPrime(num))
